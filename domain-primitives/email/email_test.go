b7d85e9bd1ef31dd0c5eff90f15192df2beef31b
zerai <teclaizerai@gmail.com>
2020-06-24 13:10:27 +0200

    Refactoring email test.

--- domain-primitives/email/email_test.go
+++ domain-primitives/email/email_test.go
@@ -6,10 +6,12 @@ import "testing"
 func TestEmail(t *testing.T)  {
 	
 	t.Run("Returns internal value", func (t *testing.T)  {
-		sut := Email{value: "pippo"}
+		value := "foo@example.com"
+		
+		email := Email{value: value}
 
-		got := sut.Value()
-		want := "pippo"
+		got := email.Value()
+		want := value
 
 		if got != want {
 			t.Errorf("got %s but want %s", got, want)
@@ -17,10 +19,12 @@ func TestEmail(t *testing.T)  {
 	})
 
 	t.Run("Returns a string value", func (t *testing.T)  {
-		sut := Email{value: "pippo"}
+		value := "foo@example.com"
+		
+		email := Email{value: value}
 
-		got := sut.String()
-		want := "pippo"
+		got := email.String()
+		want := value
 
 		if got != want {
 			t.Errorf("got %s but want %s", got, want)
@@ -28,12 +32,13 @@ func TestEmail(t *testing.T)  {
 	})
 
 	t.Run("Can be compared", func (t *testing.T)  {
-		first := Email{value: "pippo"}
-		second := Email{value: "pippo"}
+		first := Email{value: "foo@example.com"}
+		second := Email{value: "bar@example.com"}
+		copyOfFirst := Email{value: "foo@example.com"}
 
-		got := first.Equals(second)
-
-		assertTrue(t, got)
+		assertTrue(t, first.Equals(copyOfFirst))
+		assertFalse(t, first.Equals(second))
+		assertFalse(t, second.Equals(copyOfFirst))
 	})
 }
 
@@ -44,4 +49,12 @@ func assertTrue(t *testing.T, got bool)  {
 		t.Errorf("%v is not true", got)
 	}
 
+}
+
+func assertFalse(t *testing.T, got bool)  {
+	t.Helper()
+
+	if got != false {
+		t.Errorf("%v is not false", got)
+	}
 }
