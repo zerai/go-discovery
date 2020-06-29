c70df0813a10c6815f8a08389c7e2d9e620ed6c5
zerai <teclaizerai@gmail.com>
2020-06-29 10:58:20 +0200

    Add company email.

---
+++ companyemail/company_email.go
@@ -0,0 +1,111 @@
+package companyemail
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const (
+	//CompanyEmailMinLength : company email minimum lenght
+	CompanyEmailMinLength = 5
+
+	//CompanyEmailMaxLength :  company email minimum lenght
+	CompanyEmailMaxLength = 255
+)
+
+var (
+	companyDomains = [...]string{"acme.com", "acme.org", "acme-store.com"}
+
+	ErrInvalidEmail              = errors.New("Invalid Email.")
+	errCompanyEmailTooShort      = errors.New(fmt.Sprintf("Invalid email: min length allowed is %d", CompanyEmailMinLength))
+	errCompanyEmailTooLong       = errors.New(fmt.Sprintf("Invalid email: max length allowed is %d", CompanyEmailMaxLength))
+	errCompanyEmailInvalidDomain = errors.New(fmt.Sprintf("Invalid email: this email contains an invalid domain name."))
+
+	emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+//CompanyEmail : value object
+type CompanyEmail interface {
+	String() string
+}
+
+type companyEmail struct {
+	value string
+}
+
+func (p *companyEmail) String() string {
+	return p.value
+}
+
+func isValidDomainName(emailAddress string) bool {
+	components := strings.Split(emailAddress, "@")
+	emailDomain := components[1]
+	for _, domain := range companyDomains {
+		if emailDomain == domain {
+			return true
+		}
+	}
+	return false
+}
+
+// Here, NewCompanyEmail returns an interface, and not the companyEmail struct itself
+/*   func NewCompanyEmail(name string) CompanyEmail {
+	return companyEmail{
+	  value: name,
+	}
+  }
+*/
+
+// Here, NewCompanyEmail returns a pointer to companyEmail struct
+/* func NewCompanyEmail(emailAddress string) (CompanyEmail, error) {
+
+	emailAddress = strings.TrimSpace(emailAddress)
+	if len(emailAddress) == 0 {
+		return nil, ErrInvalidEmail
+	}
+
+	if len(emailAddress) < CompanyEmailMinLength {
+		return nil, errCompanyEmailTooShort
+	}
+
+	if len(emailAddress) > CompanyEmailMaxLength {
+		return nil, errCompanyEmailTooLong
+	}
+
+	if !emailRegexp.MatchString(emailAddress) {
+		return nil, ErrInvalidEmail
+	}
+
+	return &companyEmail{
+		value: emailAddress,
+	}, nil
+} */
+
+func NewCompanyEmail(emailAddress string) (CompanyEmail, error) {
+	emailAddress = strings.TrimSpace(emailAddress)
+	if len(emailAddress) == 0 {
+		return nil, ErrInvalidEmail
+	}
+
+	if len(emailAddress) < CompanyEmailMinLength {
+		return nil, errCompanyEmailTooShort
+	}
+
+	if len(emailAddress) > CompanyEmailMaxLength {
+		return nil, errCompanyEmailTooLong
+	}
+
+	if !emailRegexp.MatchString(emailAddress) {
+		return nil, ErrInvalidEmail
+	}
+
+	if !isValidDomainName(emailAddress) {
+		return nil, errCompanyEmailInvalidDomain
+	}
+
+	return &companyEmail{
+		value: emailAddress,
+	}, nil
+}
---
+++ companyemail/company_email_test.go
@@ -0,0 +1,110 @@
+package companyemail
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCompanyEmail(t *testing.T) {
+
+	t.Run("Can be created from string", func(t *testing.T) {
+
+		value := "foo@acme.com"
+
+		companyMail, err := NewCompanyEmail(value)
+		want := value
+		if err != nil {
+			t.Error("got an error but didn't expected one")
+		}
+
+		got := companyMail.String()
+
+		if got != want {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
+
+	t.Run("Empty string should returns error", func(t *testing.T) {
+		value := ""
+
+		_, err := NewCompanyEmail(value)
+
+		assertError(t, err, ErrInvalidEmail.Error())
+	})
+
+	t.Run("Short string should returns error", func(t *testing.T) {
+		value := "@a.a"
+
+		_, err := NewCompanyEmail(value)
+
+		assertError(t, err, errCompanyEmailTooShort.Error())
+	})
+
+	t.Run("Long string should returns error", func(t *testing.T) {
+		value := strings.Repeat("x", CompanyEmailMaxLength) + "@a.a"
+
+		_, err := NewCompanyEmail(value)
+
+		assertError(t, err, errCompanyEmailTooLong.Error())
+	})
+
+	t.Run("Malformed email address should returns error", func(t *testing.T) {
+		invalidEmailDataProvider := []struct {
+			malformedEmail string
+			error          error
+		}{
+			{"johndoe", ErrInvalidEmail},
+			{"johndoe.", ErrInvalidEmail},
+			{"john.doe", ErrInvalidEmail},
+			{"john.doe@", ErrInvalidEmail},
+			//{"john.doe@google", ErrInvalidEmail}, <- it's passed
+			{"john.doe@google.", ErrInvalidEmail},
+			{"@google.com", ErrInvalidEmail},
+			{"@google.", ErrInvalidEmail},
+			{"@google.foo.com", ErrInvalidEmail},
+			{"foo@google.com@google.com", ErrInvalidEmail},
+			{"john£$%&/()doe@google.", ErrInvalidEmail},
+		}
+
+		for _, tt := range invalidEmailDataProvider {
+			_, err := NewCompanyEmail(tt.malformedEmail)
+			assertError(t, err, tt.error.Error())
+		}
+	})
+
+	t.Run("Invalid domain in email address should returns error", func(t *testing.T) {
+		value := "foo@notacompanydomain.com"
+
+		_, err := NewCompanyEmail(value)
+
+		assertError(t, err, errCompanyEmailInvalidDomain.Error())
+	})
+}
+
+func assertTrue(t *testing.T, got bool) {
+	t.Helper()
+
+	if got != true {
+		t.Errorf("%v is not true", got)
+	}
+
+}
+
+func assertFalse(t *testing.T, got bool) {
+	t.Helper()
+
+	if got != false {
+		t.Errorf("%v is not false", got)
+	}
+}
+
+func assertError(t *testing.T, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
